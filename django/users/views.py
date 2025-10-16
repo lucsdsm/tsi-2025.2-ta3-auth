476103ec0d8fa9420ada2069df9e3c62528d3062
JG1511 <165173374+JG1511@users.noreply.github.com>
2025-10-16 19:42:10 +0000

    login finalizado

--- django/users/views.py
+++ django/users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from .models import User
 
 # Create your views here.
@@ -19,7 +20,8 @@ def create_user(request):
             error_message = "A senha deve ter pelo menos 8 caracteres."
         
         if error_message is None:
-            user = User(username=username, email=email, password=password)
+            user = User(username=username, email=email)
+            user.set_password(password)  # Criptografa a senha corretamente
             user.save()
             return render(request, "account/login.html", {"user": user})
     return render(request, "account/signUp.html", {"error": error_message} if error_message else {})
@@ -45,7 +47,7 @@ def update_user(request, user_id):
         if error_message is None:
             user.username = username
             user.email = email
-            user.password = password
+            user.set_password(password)  # Criptografa a senha corretamente
             user.save()
             return render(request, "users/home.html", {"user": user})
 
@@ -63,19 +65,28 @@ def delete_user(request, user_id):
 
 def user_login(request):
     if request.method == "POST":
-        email = request.POST.get("email")
+        login_input = request.POST.get("login")
         password = request.POST.get("password")
 
         error_message = None
 
-        if not email or not password:
+        if not login_input or not password:
             error_message = "Todos os campos são obrigatórios."
         else:
-            try:
-                user = User.objects.get(email=email, password=password)
-                return render(request, "users/home.html", {"user": user})
-            except User.DoesNotExist:
+            # o authenticate serve para verificar se o usuário existe
+            user = authenticate(request, username=login_input, password=password) 
+            if not user:
+                try:
+                    user_obj = User.objects.get(email=login_input)
+                    user = authenticate(request, username=user_obj.username, password=password)
+                except User.DoesNotExist:
+                    user = None
+            if user:
+                login(request, user) # cria a sessão do usuário
+                return render(request, "home.html", {"user": user})
+            else:
                 error_message = "Credenciais inválidas."
-        return render(request, "users/login.html", {"error": error_message} if error_message else {})
+            
+        return render(request, "account/login.html", {"error": error_message} if error_message else {})
 
-    return render(request, "users/login.html")
+    return render(request, "account/login.html")
